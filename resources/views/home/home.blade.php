bd3620cb0d8fd4d5dca636c48b44fd1bf014f0af
Koonac <Konac2@outlook.com>
2022-07-23 00:12:21 -0300

    test

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row p-4">
             <div class="col-12">
-                <img src="{{asset('assets/logo.svg')}}" alt="logo">
+                <img src="{{asset('assets/logo.svg')}}" alt="logo-soli">
             </div>
         </div>
 </x-layout.layoutNavBar>
